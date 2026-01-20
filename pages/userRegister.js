b78eb7190dd07b6e9be54420be456b019cb727e7
PARTHKATARIA22 <katariaparth222@gmail.com>
2026-01-21 01:20:37 +0530

    get passes,contact us, register ,login pages issues fixed

--- pages/userRegister.js
+++ pages/userRegister.js
@@ -6,8 +6,8 @@ export default function userRegister() {
     return (
         <>
             <Head>
-                <title>User Register - Anwesha 2025</title>
-                <meta name="description" content="Anwesha 2025" />
+                <title>User Register - Anwesha 2026</title>
+                <meta name="description" content="Anwesha 2026" />
                 <link rel="icon" href="./logo_no_bg.svg" />
             </Head>
 
